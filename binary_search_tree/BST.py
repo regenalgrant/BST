80faa3d2313ca6ab0f5a81bfa3588f8651899962
regenalgrant <regenal@mac.com>
2017-02-01 10:19:08 -0800

    building BST

---
+++ binary_search_tree/BST.py
@@ -0,0 +1,11 @@
+from dll import
+
+class Node :
+    def __init__(self, data, left, right, parent):
+        """Initiaizing the methods on  class node."""
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.data = None
+
+    def
